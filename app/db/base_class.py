de3342aa235779cd3d7a40f965c3ad6708ace032
Abraham <abarhamg418@gmail.com>
2026-06-21 01:06:00 -0500

    configuracion de la clase base y sesion de base de datos con sqlalchemy

---
+++ app/db/base_class.py
@@ -0,0 +1,17 @@
+
+
+from typing import Any
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+class Base(DeclarativeBase):
+    """
+    Clase base para todos los modelos de la BDs. Que da la generacion automatica del nombre de la tabla.
+    y tipado estricto compatible con SQLAlchemy 2.0.
+    """
+
+    id: Any
+
+    @declared_attr.cascading
+    
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
