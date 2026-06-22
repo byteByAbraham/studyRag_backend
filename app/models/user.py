9237b892a557f51707a219e42beab69255935f92
Abraham <abarhamg418@gmail.com>
2026-06-21 23:06:48 -0500

    implementar el modelo de user con sqlalchemy y uuidv4

---
+++ app/models/user.py
@@ -0,0 +1,41 @@
+
+import uuid
+from datetime import datetime, timezone
+from sqlalchemy import String, DateTime, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base_class import Base
+
+class User(Base):
+
+    """
+    Modelo relacional para tabla de usuarios. 
+
+    """
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        unique=True, 
+        nullable=False
+    )
+
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    full_name: Mapped[str] = mapped_column(String(255), nullable=True)
+
+
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), 
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False
+        )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), 
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False
+    )
